f9b0848da8e2d7bd1b0481d78c2612e9d3e982ea
Moucharraf <moucharaf667@gmail.com>
2023-10-12 15:03:08 +0100

    Structure des dossiers et fichiers + Theming

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,24 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#2D4356",
+        onPrimary: "#EEEEEE",
+        secondary: "#FAEAB1",
+        onSecondary: "#2D4356",
+        background: "#FFFFFF",
+        onBackground: "#000000",
+        elevation: "#f3f4f6",
+        onElevation: "#000000",
+        elevation2: "#d1d5db",
+        onElevation2:  "#000000",
+      },
+      fontFamily:{
+        "rubik": ["Rubik", "sans-serif"],
+        "young": ["Young Serif", "serif"]
+      }
+    },
   },
   plugins: [],
 }
